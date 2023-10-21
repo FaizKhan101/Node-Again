4454e1e837f7da81f9eaa1e7634a73ee626b2b57
Faiz <faizkhan8225@gmail.com>
2023-10-21 14:20:27 +0530

    Understanding different routes and post method

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
-const http = require("http")
+const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    res.write("<html><head><title>My First Page</title></head><body><h1>Hello from my node.js server!</h1></body></html>")
-})
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write(
+      '<form action="/message" method="POST"><input type="text" name="message" /><button type="submit">Send</button></form>'
+    );
+    return res.end();
+  }
 
-server.listen(3000)
+  if (url === "/message" && method === "POST") {
+    if (url === "/message" && method === "POST") {
+      fs.writeFileSync("./message.txt", "DUMMY");
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      return res.end();
+    }
+    // const message = req.message
+    // res.write(`Your message is ${message}`)
+    // return res.end()
+  }
+  console.log(req.url, req.method, req.headers);
+  res.write(
+    "<html><head><title>My First Page</title></head><body><h1>Hello from my node.js server!</h1></body></html>"
+  );
+});
 
+server.listen(3000);
