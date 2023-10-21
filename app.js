67f9636afc78488eabde231b053b24ba01e0d9bf
Faiz <faizkhan8225@gmail.com>
2023-10-21 13:55:56 +0530

    how to send a response

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require("http")
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
-    // process.exit()  
+    res.write("<html><head><title>My First Page</title></head><body><h1>Hello from my node.js server!</h1></body></html>")
 })
 
 server.listen(3000)
