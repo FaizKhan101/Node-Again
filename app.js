bd029829818b881b12881c857bb86399a3fd9d12
Faiz <faizkhan8225@gmail.com>
2023-10-21 16:16:27 +0530

    Refactor code with two files and use it

--- app.js
+++ app.js
@@ -1,36 +1,7 @@
 const http = require("http");
-const fs = require("fs");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.setHeader("Content-Type", "text/html");
-    res.write(
-      '<form action="/message" method="POST"><input type="text" name="message" /><button type="submit">Send</button></form>'
-    );
-    return res.end();
-  }
+const routes = require("./routes")
 
-  if (url === "/message" && method === "POST") {
-        const body = []
-        req.on('data', (chunk) => {
-            body.push(chunk)
-        });
-        req.on("end", () => {
-            const parsedBody = Buffer.concat(body).toString()
-            const message = parsedBody.split("=")[1]
-            fs.writeFile("./message.txt", message, (err) => {
-                res.statusCode = 302;
-                res.setHeader("Location", "/");
-                return res.end();
-            });
-        })
-    }
-//   console.log(req.url, req.method, req.headers);
-//   res.write(
-//     "<html><head><title>My First Page</title></head><body><h1>Hello from my node.js server!</h1></body></html>"
-//   );
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
