40bbd6c69ccf64b799632644f78175df5a262843
Faiz <faizkhan8225@gmail.com>
2023-10-24 12:57:34 +0530

    Add express server

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const http = require("http");
-
 const express = require("express")
 
 const app = express()
@@ -13,6 +11,6 @@ app.use((req, res, next) => {
     console.log(("In another middleware!"));
     res.send('<h1>Hello From Express</h1>')
 })
-const server = http.createServer(app);
 
-server.listen(3000, () => console.log("Server start at port 3000!"));
+
+app.listen(3000, () => console.log("Server start at port 3000!"))
