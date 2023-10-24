bb6ef7f4bfc9b044359a925665d16cbe296b9c79
Faiz <faizkhan8225@gmail.com>
2023-10-24 14:54:17 +0530

    Add more features of express js

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
+const exp = require("constants")
 const express = require("express")
 
 const app = express()
 
-app.use((req, res, next) => {
-    console.log("In the middleware!");
-    next()
+app.use(express.urlencoded({extended: false}))
+
+app.use("/add-product", (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>')
 })
 
-app.use((req, res, next) => {
+app.use("/product", (req, res, next) => {
+    const body = req.body
+    console.log(body);
+    res.redirect("/")
+})
+
+app.use("/",(req, res, next) => {
     console.log(("In another middleware!"));
     res.send('<h1>Hello From Express</h1>')
 })
