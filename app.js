e3985d67e7affe4cfe02c0c4b1b2935ea5a1fe50
Faiz <faizkhan8225@gmail.com>
2023-10-24 12:52:45 +0530

    Addingg basic middleware

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const express = require("express")
 
 const app = express()
 
+app.use((req, res, next) => {
+    console.log("In the middleware!");
+    next()
+})
+
+app.use((req, res, next) => {
+    console.log(("In another middleware!"));
+    res.send('<h1>Hello From Express</h1>')
+})
 const server = http.createServer(app);
 
 server.listen(3000, () => console.log("Server start at port 3000!"));
