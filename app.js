a9508a9c2376e5645210d5aa55b1bf1b06d4f3c6
Faiz <faizkhan8225@gmail.com>
2023-10-26 15:39:50 +0530

    Add route firtering for admin route

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const shopRoutes = require("./routes/shop")
 
 app.use(express.urlencoded({extended: false}))
 
-app.use(adminRoutes)
+app.use("/admin", adminRoutes)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
