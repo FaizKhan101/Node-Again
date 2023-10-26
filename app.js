36ac0cb27751013c52c48b90a7582bb8be759f16
Faiz <faizkhan8225@gmail.com>
2023-10-26 12:34:45 +0530

    Expand the app.js file into different routes

--- app.js
+++ app.js
@@ -1,24 +1,13 @@
-const exp = require("constants")
 const express = require("express")
 
 const app = express()
 
+const adminRoutes = require("./routes/admin")
+const shopRoutes = require("./routes/shop")
+
 app.use(express.urlencoded({extended: false}))
 
-app.use("/add-product", (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>')
-})
-
-app.use("/product", (req, res, next) => {
-    const body = req.body
-    console.log(body);
-    res.redirect("/")
-})
-
-app.use("/",(req, res, next) => {
-    console.log(("In another middleware!"));
-    res.send('<h1>Hello From Express</h1>')
-})
-
+app.use(adminRoutes)
+app.use(shopRoutes)
 
 app.listen(3000, () => console.log("Server start at port 3000!"))
