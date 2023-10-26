b979b204f3285f8afeaf851b4c59a7e13aa6e658
Faiz <faizkhan8225@gmail.com>
2023-10-26 12:39:57 +0530

    Add 404 basic middleware

--- app.js
+++ app.js
@@ -10,4 +10,8 @@ app.use(express.urlencoded({extended: false}))
 app.use(adminRoutes)
 app.use(shopRoutes)
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found!</h1>')
+})
+
 app.listen(3000, () => console.log("Server start at port 3000!"))
