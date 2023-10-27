2a28f09e7448424a0c22254364b2f56a52a26c9d
Faiz <faizkhan8225@gmail.com>
2023-10-27 15:41:57 +0530

    Add ejs templating engine

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const rootDir = require("./util/path")
 const adminData = require("./routes/admin")
 const shopRoutes = require("./routes/shop")
 
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
 app.use(express.urlencoded({extended: false}))
 app.use(express.static("public"))
 
@@ -15,7 +18,7 @@ app.use("/admin", adminData.router)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, "views", "404.html"))
+    res.status(404).render("404", { pageTitle: "Page Not Found!" })
 })
 
 app.listen(3000, () => console.log("Server start at port 3000!"))
--- routes/shop.js
+++ routes/shop.js
@@ -1,16 +1,12 @@
-const path = require("path")
-
 const express = require("express")
-
-const rootDir = require("../util/path")
-const adminData = require("./admin")
 
 const router = express.Router()
 
+const adminData = require("./admin")
 
 router.get("/",(req, res, next) => {
     console.log("shop.js", adminData.products);
-    res.sendFile(path.join(rootDir, 'views', "shop.html"))
+    res.render("shop", { pageTitle: "Shop", prods: adminData.products })
 })
 
 module.exports = router
