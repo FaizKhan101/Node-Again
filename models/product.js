6ee71ab98015a8aa74049efd34ec4a6b3e6d1b14
Faiz <faizkhan8225@gmail.com>
2023-10-28 14:01:46 +0530

    Add dynamic routes for product detial and add id property in product model

--- models/product.js
+++ models/product.js
@@ -23,6 +23,7 @@ class Product {
   }
 
   save() {
+    this.id = Math.random().toString()
     getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
