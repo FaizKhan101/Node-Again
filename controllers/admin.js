2fd13c1faa9b3f37761ed570c948e94ff44421ae
Faiz <faizkhan8225@gmail.com>
2023-10-30 13:58:51 +0530

    PrePopulate the form for editing a product

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,6 +4,7 @@ exports.getAddProduct = (req, res, next) => {
   res.render("admin/edit-product", {
     pageTitle: "Add Product",
     path: "/admin/add-product",
+    editing: false
   });
 };
 
@@ -32,9 +33,16 @@ exports.getEditProduct = (req, res, next) => {
   if (!editMode) {
     return res.redirect("/")
   }
-  res.render("admin/edit-product", {
-    pageTitle: "Edit Product",
-    path: "/admin/edit-product",
-    editing: editMode
-  });
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    if (!product) {
+      return res.redirect("/")
+    }
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: editMode,
+      product: product
+    });
+  })
 };
