75deae98205a568a243f3d06a16865c79a3ca5a2
Faiz <faizkhan8225@gmail.com>
2023-10-31 12:58:09 +0530

    Displaying a simple cart view page

--- controllers/shop.js
+++ controllers/shop.js
@@ -18,34 +18,51 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.getProduct = (req, res, next) => {
-  const prodId = req.params.productId
-  Product.findById(prodId, product => {
-    res.render('shop/product-detail', {
+  const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+    res.render("shop/product-detail", {
       path: "/products",
       pageTitle: "Product Detail",
-      product: product
-    })
-  })
-}
+      product: product,
+    });
+  });
+};
 
 exports.getCart = (req, res, next) => {
-  res.render("shop/cart", {
-    pageTitle: "Your Cart",
-    path: "/cart"
-  })
-}
+  Cart.getCart((cart) => {
+    Product.fetchAll((products) => {
+      const cartProducts = [];
+      for (const product of products) {
+        const cartProductsData = cart.products.find(
+          (prod) => prod.id === product.id
+        );
+        if (cartProductsData) {
+          cartProducts.push({
+            productData: product,
+            qty: cartProductsData.qty,
+          });
+        }
+      }
+      res.render("shop/cart", {
+        pageTitle: "Your Cart",
+        path: "/cart",
+        products: cartProducts
+      });
+    });
+  });
+};
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, product => {
-    Cart.addProduct(prodId, product.price)
-  })
-  res.redirect("/cart")
-}
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect("/cart");
+};
 
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", {
     pageTitle: "Orders",
-    path: "/orders"
-  })
-}
+    path: "/orders",
+  });
+};
--- models/cart.js
+++ models/cart.js
@@ -46,4 +46,14 @@ module.exports = class Cart {
             })
         })
     }
+
+    static getCart(cb) {
+        fs.readFile(p, (err, fileContent) => {
+            if (err) {
+                cb(null)
+            } else {
+                cb(JSON.parse(fileContent))
+            }
+        })
+    }
 }
