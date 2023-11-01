3390587367d8a8e6926087ab3bee60167fc8e10d
Faiz <faizkhan8225@gmail.com>
2023-11-01 10:04:47 +0530

    Adding a MySQL database and create a connection pool

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ const app = express();
 const errorController = require("./controllers/error");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const db = require("./util/database")
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
+
+db.execute("SELECT * FROM products")
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
