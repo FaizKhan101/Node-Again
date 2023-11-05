13a707396b96a4a666e8d7b6c1e42cef40fd9740
Faiz <faizkhan8225@gmail.com>
2023-11-05 09:55:58 +0530

    Add cart into the orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -123,6 +123,33 @@ exports.postDeleteCartItem = (req, res, next) => {
     });
 };
 
+exports.postOrder = (req, res, next) => {
+  req.user
+    .getCart()
+    .then((cart) => {
+      return cart.getProducts();
+    })
+    .then((products) => {
+      return req.user
+        .createOrder()
+        .then((order) => {
+          order.addProducts(products.map(product => {
+            product.orderItem = { quantity: product.cartItem.quantity }
+            return product
+          }));
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+    .then(result => {
+      res.redirect("/orders")
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", {
     pageTitle: "Orders",
--- routes/shop.js
+++ routes/shop.js
@@ -16,6 +16,8 @@ router.post("/cart", shopController.postCart)
 
 router.post("/cart-delete-item", shopController.postDeleteCartItem)
 
+router.post("/create-order", shopController.postOrder)
+
 router.get("/orders", shopController.getOrders)
 
 
