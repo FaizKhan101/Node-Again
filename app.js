7ca6723a9dd56fd8d295aabd618546d56c50469e
Faiz <faizkhan8225@gmail.com>
2023-11-05 09:20:53 +0530

    Add Order and order-item model

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const Product = require("./models/product");
 const User = require("./models/user");
 const Cart = require("./models/cart");
 const CartItem = require("./models/cart-item");
+const Order = require("./models/order")
+const OrderItem = require("./models/order-item")
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -39,6 +41,9 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User)
+User.hasMany(Order)
+Order.belongsToMany(Product, { through: OrderItem })
 
 sequelize
   // .sync({ force: true })
