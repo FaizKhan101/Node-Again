39ba9061815448a19c30a82fb4bcadcab53f4007
Faiz <faizkhan8225@gmail.com>
2023-11-07 09:55:32 +0530

    Add user cart functionality only for new product

--- models/user.js
+++ models/user.js
@@ -1,19 +1,38 @@
 const { ObjectId } = require("mongodb");
-const db = require("../util/database")
+const db = require("../util/database");
 
 class User {
-    constructor(username, email) {
-        this.name = username;
-        this.email = email 
-    }
+  constructor(username, email, cart, id) {
+    this.name = username;
+    this.email = email;
+    this.cart = cart;
+    this._id = id;
+  }
 
-    save() {
-        return db.getDb().collection("users").insertOne(this)
-    }
+  save() {
+    return db.getDb().collection("users").insertOne(this);
+  }
 
-    static findById(id) {
-        return db.getDb().collection("users").findOne({_id: new ObjectId(id)})
-    }
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex(cp => {
+    //     return cp._id === product._id
+    // })
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    return db
+      .getDb()
+      .collection("users")
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
+  }
+
+  static findById(id) {
+    return db
+      .getDb()
+      .collection("users")
+      .findOne({ _id: new ObjectId(id) });
+  }
 }
 
-module.exports = User
+module.exports = User;
