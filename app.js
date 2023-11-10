ae89f23b9c0339e264075db1ea5d0d116f6e325f
Faiz <faizkhan8225@gmail.com>
2023-11-10 10:22:19 +0530

    Going forward to use mongoose and connect it with our db

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose")
 
 const app = express();
 
-const db = require("./util/database");
 const errorController = require("./controllers/error");
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-const User = require("./models/user")
+// const adminRoutes = require("./routes/admin");
+// const shopRoutes = require("./routes/shop");
+// const User = require("./models/user")
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -14,24 +14,22 @@ app.set("views", "views");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
-app.use((req, res, next) => {
-  User.findById("654bbbabce973fc0b0f66e23")
-    .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+// app.use((req, res, next) => {
+//   User.findById("654bbbabce973fc0b0f66e23")
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
 
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
+// app.use("/admin", adminRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-db.mongoConnect()
-  .then((result) => {
-    app.listen(3000, () => console.log("Server start at port 3000!"));
-  })
-  .catch((err) => console.log(err));
+mongoose.connect("mongodb://localhost:27017/shop").then(result => {
+  app.listen(3000, () => console.log("Server start at port 3000!"));
+})
