5598fed2224f36704bb1afd11c722e8d93a85a50
Faiz <faizkhan8225@gmail.com>
2023-11-11 14:26:02 +0530

    Able to delete product from cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -79,7 +79,7 @@ exports.postCart = (req, res, next) => {
 exports.postDeleteCartItem = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
-    .deleteItemFromCart(prodId)
+    .removeFromCart(prodId)
     .then((result) => {
       res.redirect("/cart");
     })
