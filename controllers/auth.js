3838e0542c09c4bff9eb054ff47eb79328a27582
Faiz <faizkhan8225@gmail.com>
2023-11-15 22:36:03 +0530

    Deleting a cookie by logout button

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,22 @@
 exports.getLogin = (req, res, next) => {
-    // console.log(req.get('Cookie').split('=')[1]);
-    const isLoggedIn = req.get('Cookie').trim().split('=')[1];
-    console.log(req.session.isLoggedIn);
-    res.render("auth/login", {
-        pageTitle: "Login",
-        isAuthenticated: isLoggedIn,
-        path: "/login"
-    })
-}
+  // console.log(req.get('Cookie').split('=')[1]);
+  const isLoggedIn = req.get("Cookie").trim().split("=")[1];
+  console.log(req.session.isLoggedIn);
+  res.render("auth/login", {
+    pageTitle: "Login",
+    isAuthenticated: isLoggedIn,
+    path: "/login",
+  });
+};
 
 exports.postLogin = (req, res, next) => {
-    req.session.isLoggedIn = true
-    res.redirect("/")
-}
+  req.session.isLoggedIn = true;
+  res.redirect("/");
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    console.log(err);
+    res.redirect("/");
+  });
+};
