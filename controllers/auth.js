bc7dcd2a9a8211eb3e417f65be2cf6d30a1de889
Faiz <faizkhan8225@gmail.com>
2023-11-15 11:20:10 +0530

    Set Cookie for loggedIn user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,14 @@
 exports.getLogin = (req, res, next) => {
+    console.log(req.get('Cookie').split('=')[1]);
+    const isLoggedIn = req.get('Cookie').trim().split('=')[1];
     res.render("auth/login", {
         pageTitle: "Login",
-        isAuthenticated: req.isLoggedIn,
+        isAuthenticated: isLoggedIn.toString(),
         path: "/login"
     })
 }
 
 exports.postLogin = (req, res, next) => {
-    console.log("isLoogedIn");
-    req.isLoggedIn = true
+    res.setHeader('Set-Cookie', 'loggedIn=true')
     res.redirect("/")
 }
