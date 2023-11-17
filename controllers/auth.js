51e72c2e2d1f82a53a1ad894c7c9c4b75a540d63
Faiz <faizkhan8225@gmail.com>
2023-11-17 22:54:46 +0530

    Add login functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,8 +13,30 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  req.session.isLoggedIn = true;
-  res.redirect("/");
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email: email }).then((user) => {
+    if (!user) {
+      return res.redirect("/login");
+    }
+    bcrypt
+      .compare(password, user.password)
+      .then((doMatch) => {
+        if (doMatch) {
+          req.session.isLoggedIn = true;
+          req.session.user = user;
+          return req.session.save((err) => {
+            console.log(err);
+            return res.redirect("/");
+          });
+        }
+        res.redirect("/login");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 };
 
 exports.getSignup = (req, res, next) => {
